bd534f815b286795d7d859c2edf2dca7fc78e360
えす ESU <85072753+ss2552@users.noreply.github.com>
2025-08-12 13:56:24 +0900

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,29 +1,36 @@
-#include <string.h>
-#include <stdio.h>
 #include <3ds.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // 結果のリザルト
-extern u32 result;
+extern int result;
 // 有効又は無効か
 extern bool qtmDisabled;
+// 
+extern PrintConsole topScreenConsole;
+
+// https://github.com/devkitPro/3ds-examples/tree/master/qtm
 
 int main(void){
 
-    // init
+    // デフォルトでグラフィック初期化
 	gfxInitDefault();
-    PrintConsole topScreenConsole;
+	// コンソールの初期化
 	consoleInit(GFX_TOP, &topScreenConsole);
 
 	// 背景を色々に設定 文字色を黒に設定
     topScreenConsole.bg = CONSOLE_WHITE;
-	topScreenConsole.fg = CONSOLE_BLACK;
+	topScreenConsole.fg = 0;
+	
+	// 一応 コンソールをクリア
     consoleClear();
     //
 
     // new 3dsか確認
     s64 out;
 	if(svcGetSystemInfo(&out, 0x10001, 0) != 0){
-		printf("N3ds専用");
+		printf("N3ds専用¥n");
 		goto deinit;
 	}
 
@@ -33,19 +40,26 @@ int main(void){
 	// 初期 : 白  null
 	// 成功 : 緑     1
 	// 失敗 : 赤    -1
-	topScreenConsole.bg = result > 0 ? CONSOLE_GREEN : CONSOLE_RED;
+	if(result > 0){
+		topScreenConsole.bg = CONSOLE_GREEN;
+	}else{
+		if(result == 0){
+			printf("結果無し");
+		}
+        topScreenConsole.bg = CONSOLE_RED;
+	}
 
-	const char msg[2][7] = {
+	const char* msg[2][10] = {
 		"有効化",
 		"無効化"
-	}
+	};
 	
 	printf("QTMを%c", msg[qtmDisabled]);
 	
-	// 
-	consoleClear();
+	//
+	gfxFlushBuffers();();
 
-    // 画面を更新
+    // 画面描画が完了するまで待機
     gspWaitForVBlank();
 
     // ボタンが押されるまで待機
