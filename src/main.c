e1fd3f74eaf5474b4d5e2ed73a3fd3116dea01b6
えす ESU <85072753+ss2552@users.noreply.github.com>
2025-08-12 13:03:32 +0900

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,34 +2,40 @@
 #include <stdio.h>
 #include <3ds.h>
 
-extern u32 result = 0;
-extern bool qtmDisabled = false;
+// 結果のリザルト
+extern u32 result;
+// 有効又は無効か
+extern bool qtmDisabled;
 
-int main(void)
-{
+int main(void){
+
+    // init
+	gfxInitDefault();
+    PrintConsole topScreenConsole;
+	consoleInit(GFX_TOP, &topScreenConsole);
+
+	// 背景を色々に設定 文字色を黒に設定
+    topScreenConsole.bg = CONSOLE_WHITE;
+	topScreenConsole.fg = CONSOLE_BLACK;
+    consoleClear();
+    //
 
     // new 3dsか確認
     s64 out;
-    if(svcGetSystemInfo(&out, 0x10001, 0) != 0)
-        goto exit;
-
-    // init
-
-    PrintConsole topScreenConsole;
-	gfxInitDefault();
-	consoleInit(GFX_TOP, &topScreenConsole);
-
-    topScreenConsole.bg = CONSOLE_WHITE;
-    consoleClear();
-    
-    //
+	if(svcGetSystemInfo(&out, 0x10001, 0) != 0){
+		printf("N3ds専用");
+		goto deinit;
+	}
 
 	// QTMを有効または無効化
 	rpDoQTMPatchAndToggle();
-		
+
+	// 初期 : 白  null
+	// 成功 : 緑     1
+	// 失敗 : 赤    -1
 	topScreenConsole.bg = result > 0 ? CONSOLE_GREEN : CONSOLE_RED;
 
-	char msg[2][] = {
+	const char msg[2][7] = {
 		"有効化",
 		"無効化"
 	}
@@ -48,8 +54,6 @@ int main(void)
 deinit:
 
 	gfxExit();
-
-exlt:
 
     return 0;
 }
